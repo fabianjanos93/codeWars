32f7878c8071911c4bb6f4f31b6713886d628b7e
Janos Fabian <fabian.janos93@gmail.com>
2025-02-02 21:29:51 +0100

    first prototipe still faulty

--- src/main/java/org/example/Immortal.java
+++ src/main/java/org/example/Immortal.java
@@ -7,18 +7,21 @@ public class Immortal {
    */
   static boolean debug = false;
   public static long mod = 0;
-  public static final int SIDE_LENGTH = 40;
+  public static final int SIDE_LENGTH = 700000;
   public static final int K = 0;
 
   public static void main(String[] args) {
     long startTime = System.currentTimeMillis();
-    System.out.println(elderAge(SIDE_LENGTH, SIDE_LENGTH, K, 1000007));
-//    System.out.println(elderAge(54500, 43500, 342, 1000007)); // result: 808451
+//    System.out.println(elderAge(SIDE_LENGTH, SIDE_LENGTH + 4, K, 1000007));
+    System.out.println(elderAge(545000, 435000, 342, 1000007)); // result: 808451
     long estimatedTime = System.currentTimeMillis() - startTime;
     System.out.println("run milis:" + estimatedTime);
-    System.out.println(elderAge(SIDE_LENGTH, SIDE_LENGTH, K, 1000007));
 
-    System.out.println(highestPowerOfTwoLessThan(100));
+    startTime = System.currentTimeMillis();
+//    System.out.println(oldElderAge(SIDE_LENGTH, SIDE_LENGTH + 4, K, 1000007));
+    System.out.println(oldElderAge(545000, 435000, 342, 1000007)); // result: 808451
+    estimatedTime = System.currentTimeMillis() - startTime;
+    System.out.println("run milis:" + estimatedTime);
   }
 
   static long elderAge(long n, long m, long k, long newp) {
@@ -32,11 +35,53 @@ public class Immortal {
       n = temp;
     }
 
-    long upperTriangleSizeLimit = highestPowerOfTwoLessThan(m);
-    sum += (calculateTriangleSumByLocalMax(upperTriangleSizeLimit - 1, k) << 1) % newp;
-    return sum;
+    long highestPowerOfTwo = highestPowerOfTwoLessThan(m);
+    sum += ((calculateFullSquareRows(highestPowerOfTwo - 1, k) << 1) % newp);
+
+    long perfectSquareRows = sumOfLowerRectangle(highestPowerOfTwo, (highestPowerOfTwo * 2) - 1);
+    //Mirrored part
+    sum += (perfectSquareRows * (m - highestPowerOfTwo) << 1) % newp;
+    //Rest
+    long restOfBiggestSquare = (highestPowerOfTwo * 2) - m;
+    sum += perfectSquareRows * (Math.min(restOfBiggestSquare, n - m)) % newp;
+
+    sum += remainingTriangle(highestPowerOfTwo, n, m, k, newp) << 1;
+
+    if (restOfBiggestSquare > n - m) {
+      sum += remainingBlock(restOfBiggestSquare, n, highestPowerOfTwo, k, newp);
+    }
+    return sum % newp;
   }
 
+
+  static long remainingTriangle(long nStart, long n, long m, long k, long newp) {
+    long sum = 0;
+    for (long col = nStart; col < m; col++) {
+      for (long row = col + 1; row < n; row++) {
+        long cellValue = (col ^ row) - k;
+        if (cellValue > 0) {
+          if (row < m) {
+            cellValue = cellValue << 1;
+          }
+          sum += cellValue;
+        }
+      }
+    }
+    return sum % newp; // do it!
+  }
+
+  static long remainingBlock(long nStart, long n, long mEnd, long k, long newp) {
+    long sum = 0;
+    for (long col = 0; col < mEnd; col++) {
+      for (long row = nStart; row < n; row++) {
+        long cellValue = (col ^ row) - k;
+        if (cellValue > 0) {
+          sum += cellValue;
+        }
+      }
+    }
+    return sum % newp; // do it!
+  }
 
   static long oldElderAge(long n, long m, long k, long newp) {
     mod = newp;
@@ -46,17 +91,14 @@ public class Immortal {
       m = n;
       n = temp;
     }
-    long[] colsum = new long[SIDE_LENGTH];
     for (long col = 0; col < m; col++) {
       for (long row = col + 1; row < n; row++) {
         long cellValue = (col ^ row) - k;
         cellValue = cellValue >= 0 ? cellValue : 0;
-        String binaryString = Long.toBinaryString(cellValue);
-//        System.out.print(String.format("%2d", cellValue) + " ");
-        System.out.print(
-            " ".repeat(8 - binaryString.length()) + binaryString + "(" + String.format("%2d",
-                cellValue) + ") ");
-        colsum[(int) row] += cellValue;
+//        String binaryString = Long.toBinaryString(cellValue);
+//        System.out.print(
+//            " ".repeat(8 - binaryString.length()) + binaryString + "(" + String.format("%2d",
+//                cellValue) + ") ");
         if (cellValue > 0) {
           if (row < m) {
             cellValue = cellValue << 1;
@@ -64,44 +106,13 @@ public class Immortal {
           sum += cellValue;
         }
       }
-      System.out.println();
-      System.out.print("             ".repeat((int) col + 1));
+//      System.out.println();
+//      System.out.print("             ".repeat((int) col + 1));
     }
-
-    // Print values
-    long sumByColSoFar = 0;
-    for (int i = 0; i < SIDE_LENGTH - 1; i++) {
-      long value = colsum[i];
-      String binaryStringValue = Long.toBinaryString(value);
-      String binaryStringValuePadding =
-          " ".repeat(30 - binaryStringValue.length()) + Long.toBinaryString(value);
-      sumByColSoFar += value;
-      String binaryStringSum = Long.toBinaryString(sumByColSoFar);
-      String binaryStringSumPadding =
-          " ".repeat(30 - binaryStringSum.length()) + Long.toBinaryString(sumByColSoFar);
-      long increment = colsum[i + 1] - value;
-      System.out.println(
-          String.format("%3d", i) +
-              String.format("%10d", value) +
-              "(binary: " + binaryStringValuePadding +
-              "  to Next: " + String.format("%15d", increment) +
-              ")[SUM: " + String.format("%15d", sumByColSoFar) +
-              "(" + binaryStringSumPadding + ")]");
-    }
-    return sum % newp; // do it!
+    return sum % newp;
   }
 
-  static long elderAge2(long n, long m, long k, long newp) {
-    long sum = 0;
-    if (n < m) {
-      long temp = m;
-      m = n;
-      n = temp;
-    }
-    return 0;
-  }
-
-  static long calculateTriangleSumByLocalMax(long max, long k) {
+  static long calculateFullSquareRows(long max, long k) {
     int n = Long.toBinaryString(max).length();
     int binaryLength = ((n - 1) * 3) + 1;
     long baseTriangleSum = Long.parseLong("1".repeat(n) + "0".repeat(binaryLength - n), 2);
@@ -114,7 +125,11 @@ public class Immortal {
     return baseTriangleSum;
   }
 
-  public static int highestPowerOfTwoLessThan(int N) {
+  public static long sumOfLowerRectangle(long N, long M) {
+    return ((M - N + 1) * (N + M)) / 2;
+  }
+
+  public static long highestPowerOfTwoLessThan(long N) {
     if (N < 1) {
       return 0; // Edge case
     }

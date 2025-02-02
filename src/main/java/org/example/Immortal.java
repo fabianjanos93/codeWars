c0a65e892337948d4c8d40da02ceba5c14973394
Janos Fabian <fabian.janos93@gmail.com>
2025-02-02 19:27:27 +0100

    rewrite started to use updated calculations

--- src/main/java/org/example/Immortal.java
+++ src/main/java/org/example/Immortal.java
@@ -2,22 +2,43 @@ package org.example;
 
 public class Immortal {
 
-  public static final int SIDE_LENGTH = 200;
   /**
    * set true to enable debug
    */
   static boolean debug = false;
   public static long mod = 0;
+  public static final int SIDE_LENGTH = 40;
+  public static final int K = 0;
 
   public static void main(String[] args) {
     long startTime = System.currentTimeMillis();
-    System.out.println(elderAge(SIDE_LENGTH, SIDE_LENGTH, 0, 1000007));
+    System.out.println(elderAge(SIDE_LENGTH, SIDE_LENGTH, K, 1000007));
 //    System.out.println(elderAge(54500, 43500, 342, 1000007)); // result: 808451
     long estimatedTime = System.currentTimeMillis() - startTime;
     System.out.println("run milis:" + estimatedTime);
+    System.out.println(elderAge(SIDE_LENGTH, SIDE_LENGTH, K, 1000007));
+
+    System.out.println(highestPowerOfTwoLessThan(100));
   }
 
   static long elderAge(long n, long m, long k, long newp) {
+    mod = newp;
+    long sum = 0;
+
+    // n is always the longer side
+    if (n < m) {
+      long temp = m;
+      m = n;
+      n = temp;
+    }
+
+    long upperTriangleSizeLimit = highestPowerOfTwoLessThan(m);
+    sum += (calculateTriangleSumByLocalMax(upperTriangleSizeLimit - 1, k) << 1) % newp;
+    return sum;
+  }
+
+
+  static long oldElderAge(long n, long m, long k, long newp) {
     mod = newp;
     long sum = 0;
     if (n < m) {
@@ -29,9 +50,12 @@ public class Immortal {
     for (long col = 0; col < m; col++) {
       for (long row = col + 1; row < n; row++) {
         long cellValue = (col ^ row) - k;
+        cellValue = cellValue >= 0 ? cellValue : 0;
         String binaryString = Long.toBinaryString(cellValue);
 //        System.out.print(String.format("%2d", cellValue) + " ");
-        System.out.print(" ".repeat(8 - binaryString.length()) + binaryString + "(" + String.format("%2d", cellValue) + ") ");
+        System.out.print(
+            " ".repeat(8 - binaryString.length()) + binaryString + "(" + String.format("%2d",
+                cellValue) + ") ");
         colsum[(int) row] += cellValue;
         if (cellValue > 0) {
           if (row < m) {
@@ -46,16 +70,19 @@ public class Immortal {
 
     // Print values
     long sumByColSoFar = 0;
-    for (int i = 0 ; i < SIDE_LENGTH - 1  ; i++) {
+    for (int i = 0; i < SIDE_LENGTH - 1; i++) {
       long value = colsum[i];
       String binaryStringValue = Long.toBinaryString(value);
-      String binaryStringValuePadding = " ".repeat(30 - binaryStringValue.length()) + Long.toBinaryString(value);
+      String binaryStringValuePadding =
+          " ".repeat(30 - binaryStringValue.length()) + Long.toBinaryString(value);
       sumByColSoFar += value;
       String binaryStringSum = Long.toBinaryString(sumByColSoFar);
-      String binaryStringSumPadding = " ".repeat(30 - binaryStringSum.length()) + Long.toBinaryString(sumByColSoFar);
+      String binaryStringSumPadding =
+          " ".repeat(30 - binaryStringSum.length()) + Long.toBinaryString(sumByColSoFar);
       long increment = colsum[i + 1] - value;
       System.out.println(
-          String.format("%10d", value) +
+          String.format("%3d", i) +
+              String.format("%10d", value) +
               "(binary: " + binaryStringValuePadding +
               "  to Next: " + String.format("%15d", increment) +
               ")[SUM: " + String.format("%15d", sumByColSoFar) +
@@ -74,19 +101,28 @@ public class Immortal {
     return 0;
   }
 
-  long calculateTriangleSumByLocalMax(long max) {
-    Long.toBinaryString(max+1).length();
-    return 0;
-  }
+  static long calculateTriangleSumByLocalMax(long max, long k) {
+    int n = Long.toBinaryString(max).length();
+    int binaryLength = ((n - 1) * 3) + 1;
+    long baseTriangleSum = Long.parseLong("1".repeat(n) + "0".repeat(binaryLength - n), 2);
 
-  public long sudoRecursiveStep(long localMax, long n, long m, long previousSum) {
-    long rowMax = localMax * 2;
-    long colMax = localMax * 2 - 1;
-    if (rowMax > n || colMax > m) {
-//      TODO: SMOL GAIN
-      return -1;
+    long repetition = (max + 1) / 2;
+    for (int i = 1; i < k; i++) {
+      baseTriangleSum -= i * repetition;
     }
-    return 0;
+    baseTriangleSum -= k * repetition * (max - k + 1);
+    return baseTriangleSum;
   }
 
+  public static int highestPowerOfTwoLessThan(int N) {
+    if (N < 1) {
+      return 0; // Edge case
+    }
+    N |= (N >> 1);
+    N |= (N >> 2);
+    N |= (N >> 4);
+    N |= (N >> 8);
+    N |= (N >> 16);
+    return N - (N >> 1);
+  }
 }

9d52e4560097d34c03fbcfeb9d52f566ebab515f
Janos Fabian <fabian.janos93@gmail.com>
2025-02-08 21:51:48 +0100

    Optimisation Needed

--- src/main/java/org/example/Immortal.java
+++ src/main/java/org/example/Immortal.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import javafx.util.Pair;
+
 public class Immortal {
 
   /**
@@ -14,14 +18,13 @@ public class Immortal {
     long startTime = System.currentTimeMillis();
 //    // System.out.println(elderAge(SIDE_LENGTH, SIDE_LENGTH, K, 1000007));
 //    // System.out.println(elderAge(545, 435, 342, 1000007)); // result: 808451
-    System.out.println(elderAge(17, 6, 0, 10000000)); //result : 5
+    System.out.println(elderAge(100000000L, 50000000L, 60000, 13719506)); //result : 5
     long estimatedTime = System.currentTimeMillis() - startTime;
     System.out.println("run milis:" + estimatedTime);
 
     startTime = System.currentTimeMillis();
 //    // System.out.println(oldElderAge(SIDE_LENGTH, SIDE_LENGTH, K, 1000007));
-//    // System.out.println(oldElderAge(545, 435, 342, 1000007)); // result: 808451
-    System.out.println(oldElderAge(17, 6, 0, 10000000)); //result : 5
+//    System.out.println(oldElderAge(20, 64, 0, 10000000)); //result : 5
 
     // "TeStS:
 
@@ -46,6 +49,17 @@ public class Immortal {
     long sum = 0;
     boolean thirdSectionTopCalculated = false;
     boolean thirdSectionBottomCalculated = false;
+    long startValueTopM = 0;
+    long startValueBottomN = 0;
+    long startValueBottomM = 0;
+
+    long stage1aTime = 0;
+    long stage1bTime = 0;
+    long stage2Time = 0;
+    long stage3Time = 0;
+    long stage4Time = 0;
+
+    HashMap<Long, Long> remainingSmallerBlockToSums = new HashMap<>();
 
     // n is always the longer side
     if (n < m) {
@@ -55,109 +69,201 @@ public class Immortal {
     }
 
     for (long i = 1; i <= m; i++) {
-
       long startValue = nextPowerOfTwo(i);
       long endValueN = i - 1;
       long endValueM = i - 1;
+
       boolean setBottom = false;
       boolean setTop = false;
 
+      long startTime = System.currentTimeMillis();
       // First Section
       long nextPowerOfTwo = nextPowerOfTwo(i);
       long remainingSmallerBlocks = nextPowerOfTwo - i;
       while (remainingSmallerBlocks > 0) {
         long previousPowerOfTwo = highestPowerOfTwoLessThan(remainingSmallerBlocks);
-        if (i + remainingSmallerBlocks > n) {
+
+        stage1aTime += System.currentTimeMillis() - startTime;
+        startTime = System.currentTimeMillis();
+        long mEnd = i + remainingSmallerBlocks;
+        if (mEnd > n) {
           remainingSmallerBlocks -= previousPowerOfTwo;
           continue;
         }
         long sum1 = sumOfFullPowerBlock(previousPowerOfTwo, (previousPowerOfTwo * 2) - 1, k, newp);
-//         System.out.println(i + " " + m + " " + remainingSmallerBlocks);
-        if (i + remainingSmallerBlocks <= m) {
+        if (mEnd <= m) {
           sum1 = sum1 << 1;
           if (!setBottom) {
-            endValueM = i + remainingSmallerBlocks;
-//            System.out.println(
-//                i + " First setBottom to " + endValueM + " (" + remainingSmallerBlocks + ")");
+            endValueM = mEnd;
             setBottom = true;
           }
         }
-        if (i + remainingSmallerBlocks <= n) {
+        if (mEnd <= n) {
           if (!setTop) {
-            endValueN = i + remainingSmallerBlocks;
-//            System.out.println(
-//                i + " First setBottom to " + endValueM + " (" + remainingSmallerBlocks + ")");
+            endValueN = mEnd;
             setTop = true;
           }
         }
         sum += sum1 % newp;
-        System.out.println(
-            i + " first section: " + sum1 + "(from: " + previousPowerOfTwo + ", to: " + (
-                (previousPowerOfTwo * 2) - 1) + ")");
         remainingSmallerBlocks -= previousPowerOfTwo;
       }
 
+      stage1bTime += System.currentTimeMillis() - startTime;
+      startTime = System.currentTimeMillis();
       // Second Section
       while (nextPowerOfTwo(endValueN + 1) <= n) {
         endValueN = nextPowerOfTwo(endValueN + 1);
-        if (nextPowerOfTwo(endValueM + 1) <= m - 1) {
+        if (endValueN <= m - 1) {
 //          System.out.println(i + " Update setBottom to " + nextPowerOfTwo(endValueM + 1));
-          endValueM = nextPowerOfTwo(endValueM + 1);
+          endValueM = endValueN;
         }
       }
       if (startValue < endValueN) {
         long sum1 = sumOfFullPowerBlock(startValue, endValueN - 1, k, newp);
         long sum2 = sumOfFullPowerBlock(startValue, endValueM - 1, k, newp);
-//        // System.out.println(i + " | " + sum1 + " | " + sum2);
+//        System.out.println(
+//            i + " |sum1: " + sum1 + " (" + startValue + ", " + (endValueN - 1) + ") |sum2: "
+//                + sum2);
         sum += sum1;
         // System.out.println(i + " second section sum1 above: " + sum1);
-        sum += sum2;
+        sum += sum2 > 0 ? sum2 : 0;
         // System.out.println(i + " second section sum2 below: " + sum2);
       }
-
+      stage2Time += System.currentTimeMillis() - startTime;
+      startTime = System.currentTimeMillis();
       // Third Section
       // Bottom
-      System.out.println("endv: " + endValueN);
+//      System.out.println("endv: " + endValueN);
       if (!thirdSectionBottomCalculated && (endValueM < n)) {
         long thirdSectionBottom =
             sumOfFullPowerBlock(endValueM, endValueM * 2 - 1, k, newp) * (m - endValueM);
         sum += thirdSectionBottom;
-        System.out.println("bottom sum: "+ (m - endValueM) + " " + thirdSectionBottom);
+//        System.out.println("bottom sum: " + (m - endValueM) + " " + thirdSectionBottom);
         thirdSectionBottomCalculated = true;
       }
       // Top
-      if (!thirdSectionTopCalculated && (endValueN < m )) {
+      if (!thirdSectionTopCalculated && (endValueN < m)) {
         long thirdSectionTop =
             sumOfFullPowerBlock(endValueN, endValueN * 2 - 1, k, newp) * (n - endValueN);
-        System.out.println("top sum: "+ (n - endValueN) + " " + thirdSectionTop);
+//        System.out.println("top sum: " + (n - endValueN) + " " + thirdSectionTop);
         sum += thirdSectionTop;
         thirdSectionTopCalculated = true;
       }
+      stage3Time += System.currentTimeMillis() - startTime;
+      startTime = System.currentTimeMillis();
+
+
 
       // Fourth Section
       // Bottom
       // System.out.println("endM: " + endValueM);
+      boolean nothingLeftBottom = false;
       if (i > highestPowerOfTwoLessThan(m) || !thirdSectionBottomCalculated) {
-        for (long j = endValueM; j < m; j++) {
-          long cellValue = (i - 1 ^ j) - k;
-          if (cellValue > 0) {
-            sum += cellValue;
-            System.out.println(i + "|" + j + " cell from rest bottom: " + cellValue);
+        long startN = Math.max(endValueN, startValueBottomN);
+        if (i > startValueBottomM && startN < m) {
+          while (!nothingLeftBottom) {
+            long blockSideSize = Math.max(highestPowerOfTwoLessThan(m - startN + 1),
+                highestPowerOfTwoLessThan(m - i + 1));
+            while (startN < m) {
+              if (blockSideSize == 0) {
+                nothingLeftBottom = true;
+                break;
+              }
+              long firstValueOfBlock = ((i - 1) ^ startN) - k;
+              long lastValueOfBlock = ((i - 1) ^ (startN + blockSideSize - 1)) - k;
+              long blockSum;
+
+              if ((startN + blockSideSize - 1) <= m) {
+                if (i + blockSideSize - 1 <= m) {
+//                  System.out.println(1);
+                  blockSum = sumOfFullPowerBlock(firstValueOfBlock, lastValueOfBlock, k, newp)
+                      * blockSideSize;
+                  sum += blockSum;
+                  nothingLeftBottom = true;
+                } else {
+//                  System.out.println(2);
+//                  System.out.println("lineSum: " + lineSum + " blockSide: " +  (m - i + 1));
+                  sum += sumOfFullPowerBlock(firstValueOfBlock, lastValueOfBlock, k,
+                      newp) * (m - i + 1);
+                  nothingLeftBottom = true;
+                }
+              } else {
+                if (i + blockSideSize - 1 <= m) {
+//                  System.out.println(3);
+                  sum += sumOfFullPowerBlock(firstValueOfBlock, lastValueOfBlock, k,
+                      newp) * (m - startN);
+                  nothingLeftBottom = true;
+                } else {
+                  startValueBottomN += blockSideSize;
+                  nothingLeftBottom = false;
+                  break;
+                }
+              }
+              startN += blockSideSize;
+            }
+            startValueBottomM += blockSideSize;
           }
         }
       }
 
+
       // Top
+      boolean nothingLeftTop = false;
       if (i > highestPowerOfTwoLessThan(m) || !thirdSectionTopCalculated) {
-        for (long j = endValueN; j < n; j++) {
-          long cellValue = (i - 1 ^ j) - k;
-          if (cellValue > 0) {
-            sum += cellValue;
-            System.out.println(i + "|" + j + " cell from rest top: " + cellValue);
+        long startN = Math.max(endValueN, startValueBottomN);
+        if (i > startValueTopM && startN < n) {
+          while (!nothingLeftTop) {
+            long blockSideSize = Math.max(highestPowerOfTwoLessThan(n - startN + 1),
+                highestPowerOfTwoLessThan(m - i + 1));
+            while (startN < n) {
+              if (blockSideSize == 0) {
+                nothingLeftTop = true;
+                break;
+              }
+              long firstValueOfBlock = ((i - 1) ^ startN) - k;
+              long lastValueOfBlock = ((i - 1) ^ (startN + blockSideSize - 1)) - k;
+              long blockSum;
+
+              if ((startN + blockSideSize - 1) <= n) {
+                if (i + blockSideSize - 1 <= m) {
+//                  System.out.println(1);
+                  blockSum = sumOfFullPowerBlock(firstValueOfBlock, lastValueOfBlock, k, newp)
+                      * blockSideSize;
+                  sum += blockSum;
+                  nothingLeftTop = true;
+                } else {
+//                  System.out.println(2);
+//                  System.out.println("lineSum: " + lineSum + " blockSide: " +  (m - i + 1));
+                  sum += sumOfFullPowerBlock(firstValueOfBlock, lastValueOfBlock, k,
+                      newp) * (m - i + 1);
+                  nothingLeftTop = true;
+                }
+              } else {
+                if (i + blockSideSize - 1 <= m) {
+//                  System.out.println(3);
+                  sum += sumOfFullPowerBlock(firstValueOfBlock, lastValueOfBlock, k,
+                      newp) * (n - startN);
+                  nothingLeftTop = true;
+                } else {
+                  startValueBottomN += blockSideSize;
+                  nothingLeftTop = false;
+                  break;
+                }
+              }
+              startN += blockSideSize;
+            }
+            startValueTopM += blockSideSize;
           }
         }
       }
+
+      stage4Time += System.currentTimeMillis() - startTime;
     }
+    System.out.println("stage1aTime " + stage1aTime);
+    System.out.println("stage1bTime " + stage1bTime);
+    System.out.println("stage2Time " + stage2Time);
+    System.out.println("stage3Time " + stage3Time);
+    System.out.println("stage4Time " + stage4Time);
     return sum % newp;
   }
 
@@ -172,11 +278,10 @@ public class Immortal {
     for (long col = 0; col < m; col++) {
       for (long row = 0; row < n; row++) {
         long cellValue = (col ^ row) - k;
+        long nextCellValue = (col ^ row + 1) - k;
         cellValue = cellValue >= 0 ? cellValue : 0;
-        String binaryString = Long.toBinaryString(cellValue);
-        System.out.print(
-            " ".repeat(8 - binaryString.length()) + binaryString + "(" + String.format("%2d",
-                cellValue) + ") ");
+        System.out.print(String.format("%2d ",
+            cellValue));// + String.format("(%3d) ", nextCellValue-cellValue));
         if (cellValue > 0) {
 //          if (row < m) {
 //            cellValue = cellValue << 1;
